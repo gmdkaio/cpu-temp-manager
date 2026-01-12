490fd6dfd9aa7bee44ddb232ee8239ec36f7ab2b
gmdkaio <gmdkaio@gmail.com>
2026-01-12 00:31:15 -0300

    Optimizing read logic/preparing for live + logging function

--- main.c
+++ main.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h> // Timestamping
+#include <unistd.h> // Sleep()
 int main() {
-    FILE *file; // Pointer
-    char buffer[16]; // Buffer to hold file content(16 bytes)
-    int raw_temp; 
-    float final_temp; 
+    while(1) { // Infinite loop
+        FILE *fptr, *lptr; // File pointer(fptr) and Log pointer(lptr)
+        char buffer[16]; // Buffer to hold file content(16 bytes)
+        int raw_temp; 
+        float final_temp; 
 
-
-    // Open the temperature input file(AMD)
-    // The usual path is /sys/class/thermal/thermal_zone0/temp
-    file = fopen("/sys/class/hwmon/hwmon2/temp1_input", "r");
-    
-    if(file == NULL) { // Check if file exists
-        printf("Error...");
-        return 1; 
+        // READ LOGIC
+        
+        // Open the temperature input file(AMD), the usual path is /sys/class/thermal/thermal_zone0/temp
+        fptr = fopen("/sys/class/hwmon/hwmon2/temp1_input", "r");
+        if(fptr == NULL) return 1; // Exit if file not found
+        fgets(buffer, 16, fptr); // Read the content into buffer
+        fclose(fptr); // Close the file
+        final_temp = atof(buffer) / 1000.0; // Convert to Celsius   
     }
-
-    // Reads the temperature from file and store it in buffer
-    if(fgets(buffer, sizeof(buffer), file) != NULL){ 
-        raw_temp = atoi(buffer); // Converts temp string to int
-        final_temp = raw_temp / 1000.0; // Divides temp by 1000 to get Celsius temp
-        printf("Current Temperature: %.2f °C\n", final_temp);
-    }
-
-    // Release the file handle back to the OS 
-    fclose(file); 
-
-    return 0;
 }
